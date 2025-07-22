bec4bae7ddb877281f4276e9478c2769a05ecec5
Лера Никитина <valerianikitina56436@gmail.com>
2025-07-22 14:44:05 +0300

    add readme

--- src/main/java/com/example/auth_service/config/SecurityConfig.java
+++ src/main/java/com/example/auth_service/config/SecurityConfig.java
@@ -3,7 +3,7 @@ package com.example.auth_service.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider; // Импортируем провайдер
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
